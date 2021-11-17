e10ad8a31385400af05354df9c7c3b90bd5f6063
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2021-11-17 23:48:38 +0700

    add user agent

--- src/runner.js
+++ src/runner.js
@@ -18,7 +18,10 @@ const failed = [];
 
 async function fetch(url, proxy) {
   let ops = {
-    timeout: 10000
+    timeout: 10000,
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+    }
   };
 
   if (proxy) {
